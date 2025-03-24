8a20cf86d8d7b6892eba9b3bb19402056af6139f
Elisavet Chatzikyrka <echatzikyrka@gmail.com>
2025-03-24 22:55:09 +0200

    add least squares estimation

--- Part 1/least_squares_estimation.m
+++ Part 1/least_squares_estimation.m
@@ -1,15 +1,19 @@
 function estimations = least_squares_estimation(X)
+    %X = [q, qdot, u]
+
+    g = 9.81;
+
     % Parameter estimation
     Phi = [X(2:end-1,1) X(1:end-2,1) X(2:end-1,2)];
     y = X(3:end,1);
     theta = (Phi' * Phi) \ (Phi' * y);
 
-    L_est = 9.81 / theta(1);
+    L_est = g / theta(1);
     mL2_est = 1 / theta(3);
     c_est = theta(2) * mL2_est;
     m_est = mL2_est / (L_est^2);
 
-    A_est = [0 1; -9.81/L_est -c_est/(m_est*L_est^2)];
+    A_est = [0 1; -g/L_est -c_est/(m_est*L_est^2)];
     B_est = [0; 1/(m_est*L_est^2)];
     dxdt_est = @(t, x) A_est*x + B_est*X(t,2);
     [t_cont, X_est] = ode45(dxdt_est, 0:1e-5:20, [0; 0]);
@@ -18,6 +22,11 @@ function estimations = least_squares_estimation(X)
     qdot_est = X_est(:,2);
 
     error_est = y - q_est;
+
+    fprintf('Parameter Estimation:\n');
+    fprintf('L_est = %.4f m\n', L_est);
+    fprintf('m_est = %.4f kg\n', m_est);    
+    fprintf('c_est = %.4f Nm/sec\n', c_est);
 
     estimations = {L_est, m_est, c_est, A_est, B_est, t_cont, q_est, qdot_est, error_est};
 end
--- Part 1/quest_two.m
+++ Part 1/quest_two.m
@@ -1,5 +1,6 @@
 clc; clear; close all;
 
+% System Parameters
 m = 0.75;
 L = 1.25;
 c = 0.15;
@@ -7,11 +8,12 @@ g = 9.81;
 A0 = 4;
 omega = 2;
 
+% System Dynamics
 A = [0 1; -g/L -c/(m*L^2)];
 B = [0; 1/(m*L^2)];
 
-Ts = 0.1;
-t_sim = 0:Ts:20;
+T_sample = 0.1; 
+t_sim = 0:T_sample:20;
 dt = 1e-4;
 t_cont = 0:dt:20;
 
@@ -25,38 +27,15 @@ qdot = X(:,2);
 % add custom function
 q_samples = interp1(t_cont, q, t_sim);
 qdot_samples = interp1(t_cont, qdot, t_sim);
-qddot_samples = gradient(qdot_samples, Ts);
+qddot_samples = gradient(qdot_samples, T_sample);
 u_samples = u(t_sim);
 
-Phi = [-q_samples', -qdot_samples', u_samples']; % Regression matrix
-y = qddot_samples'; % Observed acceleration
-
-% Parameter estimation
-theta = (Phi' * Phi) \ (Phi' * y);
-
-L_est = g / theta(1);
-mL2_est = 1 / theta(3);
-c_est = theta(2) * mL2_est;
-m_est = mL2_est / (L_est^2);
-
-fprintf('Parameter Estimation:\n');
-fprintf('L_est = %.4f m\n', L_est);
-fprintf('m_est = %.4f kg\n', m_est);
-fprintf('c_est = %.4f Nm/sec\n', c_est);
-
-A_est = [0 1; -g/L_est -c_est/(m_est*L_est^2)];
-B_est = [0; 1/(m_est*L_est^2)];
-dxdt_est = @(t, x) A_est*x + B_est*u(t);
-[t_cont, X_est] = ode45(dxdt_est, t_cont, [0; 0]);
-
-q_est = X_est(:,1);
-qdot_est = X_est(:,2);
-
-error_est = q - q_est;
+Q = [q_samples', qdot_samples', u_samples'];
+estimations = least_squares_estimation(Q);
 
 figure;
 plot(t_cont, q, 'b', 'LineWidth', 2); hold on;
-plot(t_cont, q_est, 'g--', 'LineWidth', 2);
+plot(t_cont, estimations(6), 'g--', 'LineWidth', 2);
 xlabel('Time [sec]'); ylabel('q(t) [rad]');
 legend('Real', 'Estimated');
 title('Deflection Angle Comparison');
@@ -64,7 +43,7 @@ grid on;
 
 figure;
 plot(t_cont, qdot, 'b', 'LineWidth', 2); hold on;
-plot(t_cont, qdot_est, 'g--', 'LineWidth', 2);
+plot(t_cont, estimations(7), 'g--', 'LineWidth', 2);
 xlabel('Time [sec]'); ylabel('qdot(t) [rad/sec]');
 legend('Real', 'Estimated');
 title('Angular Velocity Comparison');
